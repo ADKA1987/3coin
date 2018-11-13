298ce8b5e808a969be57652ee9169f9059e03221
Alaa <alaa.alaleiwi@tre.se>
2018-11-13 16:46:31 +0100

    add new things
    add new things

--- app/src/main/java/se/tre/a3coin/MainActivity.java
+++ app/src/main/java/se/tre/a3coin/MainActivity.java
@@ -9,35 +9,45 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 import se.tre.a3coin.Domain.My3CoinResponse;
 import se.tre.a3coin.Service.Get3CoinRequest;
 
 public class MainActivity extends AppCompatActivity {
-    final String[] peronalNumber = new String[1];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button getMyCoin = findViewById(R.id.getMyCoin);
         final EditText personalId= findViewById(R.id.personal_id);
-        final TextView resutltext = findViewById(R.id.text);
+        final TextView resutltext = findViewById(R.id.textC3oins);
+        final TextView expiryDate = findViewById(R.id.expiryDate);
         getMyCoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //peronalNumber[0] = personalId.getText().toString();
                 try {
-                    resutltext.setText( getCoins(personalId.getText().toString()));
+                    My3CoinResponse my3CoinResponse = null;
+
+                    my3CoinResponse =  getCoins(personalId.getText().toString());
+                    if(null!=my3CoinResponse){
+                        resutltext.setText(my3CoinResponse.getCoins());
+                        expiryDate.setText(my3CoinResponse.getExpiryDate());
+                    } else {
+                        resutltext.setText("No Coins were found");
+                        expiryDate.setText(",,,,");
+                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
-    private String getCoins(String personalId) throws IOException {
+    private My3CoinResponse getCoins(String personalId) throws IOException {
 
 
-        String my3CoinResponse = null;
+        My3CoinResponse my3CoinResponse = null;
         if(null == personalId || personalId.isEmpty()) {
             Toast.makeText(this, "Personal number cannot be empty", Toast.LENGTH_LONG).show();
             return my3CoinResponse;
@@ -45,8 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         Get3CoinRequest get3CoinRequest = new Get3CoinRequest();
-        get3CoinRequest.getCoins(personalId);
-        //my3CoinResponse = get3CoinRequest.getMy3Coins();
+        try {
+            my3CoinResponse =  get3CoinRequest.execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         return my3CoinResponse;
     }
--- app/src/main/java/se/tre/a3coin/Service/Get3CoinRequest.java
+++ app/src/main/java/se/tre/a3coin/Service/Get3CoinRequest.java
@@ -1,23 +1,26 @@
 package se.tre.a3coin.Service;
 
+import org.ksoap2.HeaderProperty;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapPrimitive;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
+
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.widget.Toast;
 
-public class Get3CoinRequest{
+import java.util.ArrayList;
 
-    public Get3CoinRequest(){
+import se.tre.a3coin.Domain.My3CoinResponse;
 
-    }
+public class Get3CoinRequest  extends AsyncTask<String, String , My3CoinResponse > {
 
-    private final String NAMESPACE = "http://x12195tzz.testaccess.net:8105/";
-    private final String URL = "http://x12802tzz.testaccess.net:8237/Services/ICC2/v3?wsdl";
-    private final String SOAP_ACTION = "http://x12195tzz.testaccess.net:8105/xml/genericServlet";
-    private final String METHOD_NAME = "genericServlet";
-
+    private final String NAMESPACE = "https://fastvedio.herokuapp.com/search/";
+    private final String URL = "https://fastvedio.herokuapp.com/";
+    private final String SOAP_ACTION = "https://fastvedio.herokuapp.com/search/alaa";
+    private final String METHOD_NAME = "search";
     private String webResponse = "";
 
     private Thread thread;
@@ -25,35 +28,39 @@ public class Get3CoinRequest{
     private String Webresponse = "";
     private Handler handler = new Handler();
 
-    public void getCoins(String personalId){
-        thread = new Thread(){
-            public void run(){
-                try{
-                    SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
-                    SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
-                    //envelope.dotNet = true;
-                    envelope.setOutputSoapObject(request);
-                    HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
-                    androidHttpTransport.call(SOAP_ACTION, envelope);
-                    SoapPrimitive response = (SoapPrimitive)envelope.getResponse();
-                    Webresponse = response.toString();
-                }
+    public Get3CoinRequest(){
 
-                catch(Exception e){
-                    e.printStackTrace();
-                }
-
-                handler.post(createUI);
-            }
-        };
-
-        thread.start();
     }
-        final Runnable createUI = new Runnable() {
-    public void run(){
-        System.out.println(Webresponse);
-           }
-           };
+
+    @Override
+    protected My3CoinResponse doInBackground(String... personalId) {
+        My3CoinResponse result = null;
+        String xml="xml\t<?xml version=\"1.0\"?> <getAvailable3CoinsRequest>     <country>SE</country>     <taxpayerId>"+personalId+"</taxpayerId>     <customerType>consumer</customerType> </getAvailable3CoinsRequest>\tQUERY\tRESOURCE";
+        SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+        ArrayList<HeaderProperty> headerProperties = new ArrayList<HeaderProperty>();
+        headerProperties.add(new HeaderProperty("Proxy-Remote-User","supash"));
+        headerProperties.add(new HeaderProperty("xml",xml));
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+
+        envelope.setOutputSoapObject(request);
+        envelope.dotNet = true;
+
+        try {
+            HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+
+            androidHttpTransport.call(SOAP_ACTION, envelope,headerProperties);
+
+            SoapObject  soapResult = (SoapObject )envelope.getResponse();
+            if (soapResult != null) {
+                result = new My3CoinResponse(soapResult.getProperty("coins").toString(),soapResult.getProperty("expiryDate").toString());
+            } else {
+                result = new My3CoinResponse(null,null);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
 
