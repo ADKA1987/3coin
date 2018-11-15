e7094c6792bff234fa1e5f9c5249bf27df4c334f
AlaaAlkassar <eng.alaa87@yahoo.com>
2018-11-15 22:07:30 +0100

    Refactoring

--- app/src/main/java/se/tre/a3coin/MainActivity.java
+++ app/src/main/java/se/tre/a3coin/MainActivity.java
@@ -5,66 +5,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
 
+import se.tre.a3coin.Domain.Credit;
+import se.tre.a3coin.Domain.CreditHistoryList;
 import se.tre.a3coin.Domain.My3CoinResponse;
-import se.tre.a3coin.Service.Get3CoinRequest;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        Intent intnet = getIntent();
+        My3CoinResponse my3CoinResponse = (My3CoinResponse) intnet.getSerializableExtra("my3CoinResponse");
+        String coins = "0";
+        String expiryDate ="YYYY-MM-DD";
+       /* if (null!=my3CoinResponse.getCoins())
+            coins =my3CoinResponse.getCoins();
 
-        Button getMyCoin = findViewById(R.id.getMyCoin);
+        if (null!=my3CoinResponse.getExpiryDate())
+            expiryDate =my3CoinResponse.getExpiryDate();
+*/
 
-        final EditText personalId= findViewById(R.id.personal_id);
-        getMyCoin.setOnClickListener(new View.OnClickListener() {
+        //final CreditHistoryList listCredit = new CreditHistoryList(my3CoinResponse.getCreditHistoryList());
+
+        TextView coinsView =  findViewById(R.id.available_Coins);
+        TextView expiryDateView =  findViewById(R.id.expiry_Date);
+
+        Button historyButton = findViewById(R.id.getHistory);
+        historyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    getCoins(personalId.getText().toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                sendToHistoryActivity(null);
             }
         });
-
-
+        Button offersBtn = findViewById(R.id.getOffers);
+        offersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOffersView();
+            }
+        });
+        coinsView.setText(coins);
+        expiryDateView.setText(expiryDate);
     }
 
-
-    private void getCoins(String personalId) throws IOException {
-
-
-        My3CoinResponse my3CoinResponse = null;
-        if(null == personalId || personalId.isEmpty()) {
-            Toast.makeText(this, "Personal number cannot be empty", Toast.LENGTH_LONG).show();
-            return;
-        }
-
-         Get3CoinRequest get3CoinRequest = new Get3CoinRequest();
-        /*try {
-            my3CoinResponse =  get3CoinRequest.execute(personalId).get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-        if(null!=my3CoinResponse){
-            Intent intent = new Intent(this, Main2Activity.class);
-            intent.putExtra("my3CoinResponse",my3CoinResponse);
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Please pay your invoice to get 3Coins.", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, Main2Activity.class);
-            intent.putExtra("my3CoinResponse",my3CoinResponse);
-            startActivity(intent);
-        }
-        //return my3CoinResponse;
+    public void sendToHistoryActivity(CreditHistoryList listCredit){
+        Intent intent = new Intent(this, HistoryActivity.class);
+        //intent.putExtra("creditHistoryList",listCredit);
+        startActivity(intent);
     }
+
+    private void openOffersView() {
+        Intent intent = new Intent(this, OffersActivity.class);
+        //intent.putExtra("offersList",offersList);
+        //intent.putExtra("personalId",personalId);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/se/tre/a3coin/StartActivity.java
@@ -0,0 +1,69 @@
+package se.tre.a3coin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import se.tre.a3coin.Domain.My3CoinResponse;
+import se.tre.a3coin.Service.Get3CoinRequest;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button getMyCoin = findViewById(R.id.getMyCoin);
+
+        final EditText personalId= findViewById(R.id.personal_id);
+        getMyCoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getCoins(personalId.getText().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+
+
+    private void getCoins(String personalId) throws IOException {
+
+
+        My3CoinResponse my3CoinResponse = null;
+        if(null == personalId || personalId.isEmpty()) {
+            Toast.makeText(this, "Personal number cannot be empty", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+         Get3CoinRequest get3CoinRequest = new Get3CoinRequest();
+        /*try {
+            my3CoinResponse =  get3CoinRequest.execute(personalId).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        if(null!=my3CoinResponse){
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("my3CoinResponse",my3CoinResponse);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Please pay your invoice to get 3Coins.", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("my3CoinResponse",my3CoinResponse);
+            startActivity(intent);
+        }
+        //return my3CoinResponse;
+    }
+}
